f9b6da9c0fe7a41332dfefaeb85487f84925ec72
rachidamrani <rachid3alaoui@gmail.com>
2024-09-27 15:39:21 +0200

    dockerize a python terminal app

---
+++ docker-example-03/rng.py
@@ -0,0 +1,11 @@
+from random import randint
+
+min_number = int(input("Enter min number: "))
+max_number = int(input("Enter max number: "))
+
+
+if (max_number < min_number):
+    print("Invalid input - shutting down ...")
+else :
+    rnd_number = randint(min_number, max_number)
+    print(rnd_number)
